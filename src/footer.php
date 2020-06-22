f2be718dfc14c1d3d7c1ef283d90c923e0b65599
Cuong <bdcuong91@gmail.com>
2020-06-22 20:58:31 +0200

    footer completed

--- src/footer.php
+++ src/footer.php
@@ -1,6 +1,44 @@
     <!-- footer -->
     <footer>
-      <h2 class="font-rale">Footer</h2>
+      <div id="footer-grid">
+        <div class="footer-info">
+          <div class="footer-info-box font-size-14 font-rale">
+            <div class="email fit">
+              <span>bd.cuong@stud.fh-sm.de</span>
+              <br>
+              <span>0049 (123) 456 78 95</span>
+            </div>
+            <div class="address fit">
+              <span>Blechhammer 02</span>
+              <br>
+              <span>Schmalkalden 98574</span>
+              <br>
+              <span>Thüringen, Gemany</span>
+            </div>
+          </div>
+        </div>
+        <div class="footer-social">
+          <div class="footer-social-box font-size-20">
+            <span>
+              <a href="https://github.com/cuongduc91">
+              <i class="fa fa-github"></i>
+              </a>
+              </span>
+            <span><i class="fa fa-instagram"></i></span>
+            <span><i class="fa fa-facebook"></i></span>
+            <span><i class="fa fa-linkedin"></i></span>
+            <span><i class="fa fa-youtube"></i></span>
+            <span><i class="fa fa-twitter"></i></span>
+          </div>
+          <div class="copyright">
+            <p class="font-rale font-size-16">
+              &copy; Copyright 2020. Developed by 
+              <a href="#" style="color:#51eaea;">Cuong Duc</a>
+            </p>
+        </div>
+        </div>
+        <div class="footer-decor"></div>
+      </div>
     </footer>
     <!-- footer -->
   </div>
