6fe6254b3829f06fc341ccc91717406637acab57
Cuong <bdcuong91@gmail.com>
2020-06-22 19:32:43 +0200

    distribute pages with php

---
+++ src/footer.php
@@ -0,0 +1,8 @@
+    <!-- footer -->
+    <footer>
+      <h2 class="font-rale">Footer</h2>
+    </footer>
+    <!-- footer -->
+  </div>
+</body>
+</html>
---
+++ src/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Whatever</title>
+  <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+</head>
+
+<body>
+  <div class="" id="Grid">
+    <!-- header -->
+    <header>
+      <div class="header-title font-baloo fit">
+        <h1 style="margin-left:.8em;">
+        <a class="header-link" href="index.php"> Mobile Shopping</a>
+        </h1>
+      </div>
+    </header>
+    <!-- !header -->
+    <!-- navigation -->
+    <nav>
+      <div style="margin-top:2em;" class="navigation font-rale">
+        <h2  class="fit" style="margin-left:1em;">Navigation</h2>
+        <ul class="nav">
+          <li><a href="index.php">About us</a></li>
+          <li><a href="product.php">Products</a></li>
+          <li><a href="my-account.php">Your account</a></li>
+        </ul>
+      </div>
+    </nav>
+    <!-- !navigation -->
