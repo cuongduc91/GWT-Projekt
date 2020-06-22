23dc215a9dc411985377c14544a38f21205cebb9
Cuong <bdcuong91@gmail.com>
2020-06-22 19:37:57 +0200

    add my-account and product

---
+++ src/product.php
@@ -0,0 +1,12 @@
+<?php include("header.php") ?>
+    <!-- main -->
+    <main >
+      <section style="margin-top:2em; margin-left:2em;" id="section-about">
+        <div class="section-about font-rale">
+          <h2>Products</h2>
+          <p class="font-size-14 font-rubik">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque voluptas dolore a reprehenderit suscipit fugit corporis sed repellendus optio, doloremque vero ipsam? Nemo fugit consequuntur hic consequatur, voluptates corrupti iusto.</p>
+        </div>
+      </section>
+    </main>
+    <!-- !main -->
+<?php include("footer.php") ?>
