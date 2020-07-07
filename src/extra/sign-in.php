f855ec85a915e9a225e1a8d162cdf1c4494eefaf
Cuong <bdcuong91@gmail.com>
2020-07-07 10:25:53 +0200

    sign-in with PDO done

--- src/extra/sign-in.php
+++ src/extra/sign-in.php
@@ -1,16 +1,20 @@
 <?php 
 include ("extra/connection.php");
 if(isset($_POST['sign_in'])){
-  $name=htmlentities(mysqli_real_escape_string($con,$_POST['user_name']));
-  $pass=htmlentities(mysqli_real_escape_string($con,$_POST['user_pass']));
-  $select_user = "select * from user where user_name = '$name' and user_pass='$pass'";
-  $query = mysqli_query($con,$select_user);
-  $check_user = mysqli_num_rows($query);
-  if($check_user==1){
+  $name=$_POST['user_name'];
+  $pass=$_POST['user_pass'];
+  $select_user_sql = "select * from mobileshop.user where user_name = :name and user_pass= :pass";
+  $select_user_stmt = $con->prepare($select_user_sql);
+  $select_user_stmt->bindParam(':name', $name);
+  $select_user_stmt->bindParam(':pass', $pass);
+  $select_user_stmt->execute();
+  $select_user=$select_user_stmt->rowCount();
+  if($select_user==1){
     echo "<script>window.open('account.php','_self')</script>";
   } else {
     echo "<script>window.open('not-found.php','_self')</script>";
   }
+
 }
 
 ?>
